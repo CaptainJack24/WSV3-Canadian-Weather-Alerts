37e86721cb2307db584728cb0aa6f3f816c8da23
EC2 Default User <ec2-user@ip-172-31-32-115.us-east-2.compute.internal>
2023-01-17 23:04:28 +0000

    Updated Cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,7 +1,9 @@
-cd /home/ec2-user/WSV3-Canadian-Weather-Alerts/resources
+cd /home/ec2-user/WSV3-Canadian-Weather-Alerts
+git reset --hard HEAD~1
+cd ./resources
 rm *.txt
 cd ..
 /usr/local/bin/python3.10 ./parse_alerts.py
 git add .
 git commit -m "Updating Weather Alerts"
-git push
+git push -f
